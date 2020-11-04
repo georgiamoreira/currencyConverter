03333428f4f397393fb428a329d817309ff278f2
almeidacavalcante <jalmeidacn@gmail.com>
2020-11-04 17:51:27 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
+ <!-- sempre esse template no inicio -->
 <template>
+ <!-- sempre essa div no inicio -->
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
